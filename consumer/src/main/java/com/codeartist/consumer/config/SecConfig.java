149898a5874a78f15eed006c9ac82fa656fd1ee2
Samyak475 <sasamyakjy25@gmail.com>
2025-09-15 23:38:18 +0530

    Added implementation of RestClient

--- consumer/src/main/java/com/codeartist/consumer/config/SecConfig.java
+++ consumer/src/main/java/com/codeartist/consumer/config/SecConfig.java
@@ -2,13 +2,14 @@ package com.codeartist.consumer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class SecConfig {
 
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestClient restClient(){
+        return RestClient.create();
     }
 }
--- consumer/src/main/java/com/codeartist/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/codeartist/consumer/controller/ConsumerController.java
@@ -1,28 +1,36 @@
 package com.codeartist.consumer.controller;
 
+import jdk.jfr.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.sql.SQLOutput;
 
 @RestController
 @RequestMapping("/consumer")
 public class ConsumerController {
     @Autowired
-    RestTemplate restTemplate;
+    RestClient restClient;
     @GetMapping("/{id}")
     public ResponseEntity<String> getProducerFrmConsumer(@PathVariable String id){
-        String producer = "http://localhost:8081/producer/"+id;
-        String response = restTemplate.getForObject(producer, String.class);
-        System.out.println("Inside consumser got producer code "+ response);
-        return ResponseEntity.ok("called producer");
+        String response1 = restClient.get()
+                .uri("http://localhost:8082/producer/"+id)
+
+                .retrieve().onStatus(response -> {
+                    if(response.getStatusCode().is4xxClientError()){
+                        throw new IOException("Throw exception");
+                    }
+                    return false ;
+                }).body(String.class);
+
+        return ResponseEntity.ok("called producer"+response1);
     }
 
 }
