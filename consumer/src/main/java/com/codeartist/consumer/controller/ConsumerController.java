3a3f98f009f50bc835622c664caaaca4eca8ba23
Samyak475 <sasamyakjy25@gmail.com>
2025-09-22 23:10:17 +0530

    added this branch to implement api gateway

--- consumer/src/main/java/com/codeartist/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/codeartist/consumer/controller/ConsumerController.java
@@ -19,19 +19,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/consumer")
 public class ConsumerController {
-    @Autowired
-    RestTemplate restTemplate;
 
-    @Autowired
-    DiscoveryClient discoveryClient;
 
     @GetMapping("/{id}")
     public ResponseEntity<String> getProducerFrmConsumer(@PathVariable String id){
-//        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("producer-service");
-//        URI  uri = serviceInstances.get(0).getUri();
-        String response = restTemplate.getForObject("http://producer-service"+"/producer/"+id,String.class);
-
-        return ResponseEntity.ok("called producer"+ response);
+        return ResponseEntity.ok("called Consumer from api gateway");
     }
 
 }
